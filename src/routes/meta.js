585ff77350b3e85b27e734cdc4fcce1e97997ddf
dk <dk@Users-MacBook-Pro.local>
2022-03-27 01:03:39 +0200

    Alarm commit.

--- src/routes/meta.js
+++ src/routes/meta.js
@@ -14,74 +14,10 @@ router.get("/", asyncErrorHandler(async function (req, res) {
         let re = await extractCurrencies()
         console.log(11);
     } catch (err) {
-        
+        console.log(err);
     }
     // let dto = new ResponseDTO();
     // let item = new ExchangeRateDTO();
 }));
-
-// router.get("/", asyncErrorHandler(async function (req, res) {
-//     const time = req.query.time;
-//     let response = new ExchangeRateDTO();
-
-//     // const ip = req.params.ip;
-//     // const country = await countryExplore(ip);
-
-//     // if (null != country) {
-//     //     res.status(200).send(new CountryDTO(country).response);
-//     // } else {
-//     //     return res.status(404).send({});
-//     // }
-// }));
-
-// GET /v1/exchanges/{exchange_id}
-// GET /v1/exchanges?filter_exchange_id={filter_exchange_id}
-
-// curl https://rest.coinapi.io/v1/exchanges \
-//   --request GET 
-//   --header "X-CoinAPI-Key: 73034021-THIS-IS-SAMPLE-KEY"
-// 904852FA-4D54-433A-AB3A-7029253B658C
-
-// curl https://rest.coinapi.io/v1/exchanges \
-//   --request GET \
-//   --header "X-CoinAPI-Key: 904852FA-4D54-433A-AB3A-7029253B658C"
-
-// GET /v1/exchanges
-// [
-//     {
-//     "exchange_id": "OKCOIN_CNY",
-//     "website": "https://www.okcoin.cn/",
-//     "name": "OKCoin CNY",
-//     "data_start": "2013-06-12",
-//     "data_end": "2018-03-09",
-//     "data_quote_start": "2015-02-15T12:53:50.3430000Z",
-//     "data_quote_end": "2018-03-09T23:34:52.5800000Z",
-//     "data_orderbook_start": "2015-02-15T12:53:50.3430000Z",
-//     "data_orderbook_end": "2018-03-09T23:34:52.5800000Z",
-//     "data_trade_start": "2013-06-12T14:24:24.0000000Z",
-//     "data_trade_end": "2017-11-01T16:30:39.7077259Z",
-//     "data_symbols_count": 2,
-//     "volume_1hrs_usd": 0.0,
-//     "volume_1day_usd": 0.0,
-//     "volume_1mth_usd": 0.0
-//     },
-//     {
-//     "exchange_id": "HUOBI",
-//     "website": "https://www.huobi.com/",
-//     "name": "Huobi (HBUS)",
-//     "data_start": "2015-03-29",
-//     "data_end": "2019-11-03",
-//     "data_quote_start": "2015-03-29T21:46:06.2630000Z",
-//     "data_quote_end": "2019-11-03T18:22:29.1837496Z",
-//     "data_orderbook_start": "2015-03-29T21:46:06.2630000Z",
-//     "data_orderbook_end": "2019-11-03T18:23:53.2859878Z",
-//     "data_trade_start": "2015-03-29T21:46:08.7030000Z",
-//     "data_trade_end": "2019-11-03T18:21:48.2770000Z",
-//     "data_symbols_count": 403,
-//     "volume_1hrs_usd": 1605.80,
-//     "volume_1day_usd": 59957.44,
-//     "volume_1mth_usd": 1259508.43
-//     }
-// ]
 
 export default router;
--- src/service/coinapiproxy.js
+++ src/service/coinapiproxy.js
@@ -3,37 +3,38 @@ import util from 'util';
 import config from '../../config/config.js';
 import RateEvent from "../events/types/rate-event.js";
 import { EventEmitter } from "../event-emitter.js";
+import ExchangeDTO from "../dto/meta/ExchangeDTO.js";
 
 const instance = axios.create({
-    baseURL: 'https://rest.coinapi.io/v1',
-    timeout: 10,
-    headers: { 'X-CoinAPI-Key': config.coinapi.apikey }
+    baseURL: 'https://rest.coinapi.io',
+    timeout: 10000,
+    headers: {  'X-CoinAPI-Key' : '904852FA-4D54-433A-AB3A-7029253B658C'}
 });
 
-  
-// const options = {
-//     hostname: 'rest.coinapi.io/v1',
-//     port: 443,
-//     path: '/',
-//     method: 'GET',
-//     headers: { 'X-CoinAPI-Key': config.coinapi.apikey },
-// };
-
-// GET /v1/exchanges
-// let dto = new ResponseDTO();
-// let item = new ExchangeRateDTO();
-
 /**
- * @return ResponseDTO
+ * @return ResponseDTO[]
  */
 export async function extractCurrencies() {
-    let path = 'rest.coinapi.io/v1/exchanges';
-    let localOptions = options;
-    localOptions.path = path;
+    let path = '/v1/exchanges';
+    let result = await extract(path);
 
-    let result = await extract(localOptions);
-    console.log(result);
-    process.exit(1);
+    return result.data.map( item => new ExchangeDTO(
+        item.exchange_id,
+        item.website,
+        item.name,
+        item.data_start,
+        item.data_end,
+        item.data_quote_start,
+        item.data_quote_end,
+        item.data_orderbook_start,
+        item.data_orderbook_end,
+        item.data_trade_start,
+        item.data_trade_end,
+        item.data_symbols_count,
+        item.volume_1hrs_usd,
+        item.volume_1day_usd,
+        item.volume_1mth_usd
+    ));
 }
 
 // export
@@ -44,7 +45,7 @@ export async function extractCurrencies() {
  * @return ExchangeRateDTO
  */
 export async function extractExchangeRate(assetIdBase, assetIdQuote, time = null) {
-    const path = `rest.coinapi.io/v1/exchangerate/${assetIdBase}/${assetIdQuote}`;
+    const path = `/v1/exchangerate/${assetIdBase}/${assetIdQuote}`;
     const params = {}
 
     if (null !== time) {
@@ -52,22 +53,27 @@ export async function extractExchangeRate(assetIdBase, assetIdQuote, time = null
     }
 
     let result = await extract(path, params);
+
+    console.log(1);
+    // GET /v1/exchanges
+    // let dto = new ResponseDTO();
+    // let item = new ExchangeRateDTO();
 }
 
 async function extract(path, parameters = {}) {
     try {
         console.log(0);
 
-       let response = await axios.get(path, { params: parameters })
+     return  await instance.get(path, { params: parameters })
           .then(function (response) {
                 return response;
           })
           .catch(function (error) {
             console.log(2);
-          })
-          .then(function () {
-            console.log(3);
-          });  
+          });
+        //   .then(function () {
+        //     console.log(3);
+        //   });  
         
         console.log(4);
 
@@ -116,10 +122,13 @@ async function extract(path, parameters = {}) {
         //   });
 
         //   request.end();
+
+        return res;
     } catch (err) {
 
         console.log(22);
     }
 
  
+    return 0;
 };
